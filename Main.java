dc4df36d329d16a4d1706a572ee637f8685096c6
Zach <zachary.dowling2027@gbwl.org>
2025-09-11 18:24:58 +0000

    1.1 notes

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+// Every Java file is a CLASS
 public class Main {
-
+   // main method is the ENTRYWAY to the program
    public static void main(String []args) {
       System.out.println("Hello World");
+      System.out.println("hiii");
    }
 }
+ 
