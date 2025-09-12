62016b0d4ffba754d10b26dc2db514b696233ab8
Zach <zachary.dowling2027@gbwl.org>
2025-09-12 12:56:07 +0000

    1.2 Notes

--- Main.java
+++ Main.java
@@ -4,6 +4,21 @@ public class Main {
    public static void main(String []args) {
       System.out.println("Hello World");
       System.out.println("hiii");
+
+      // DECLARE a new variable
+      int score;
+      // ASSIGN a value to the variable
+      score = 72;
+      // INITIALIZE combines declaring + assigning
+      String pokemon = "Lucario"
+      double healthPercent = 0.75;
+      boolean isAlive = true;
+
+      // Use the plus sign to CONCATENATE (combine) Strings
+      String greeting = "Hello" + "World";
+      System.out.println(greeting);
+      //Quotation marks contain STRING LITERALS 
+      
    }
 }
  
