9129244e5878a87f5cbc6f399a29cbcdc924afa4
Zach <zachary.dowling2027@gbwl.org>
2025-09-15 13:40:40 +0000

    1.3 Notes

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
       // ASSIGN a value to the variable
       score = 72;
       // INITIALIZE combines declaring + assigning
-      String pokemon = "Lucario"
+      String pokemon = "Lucario";
       double healthPercent = 0.75;
       boolean isAlive = true;
 
@@ -18,6 +18,36 @@ public class Main {
       String greeting = "Hello" + "World";
       System.out.println(greeting);
       //Quotation marks contain STRING LITERALS 
+
+
+         //OUTPUT: difference btwn .print and .println
+         System.out.println("there");
+         System.out.print("Hi");
+         System.out.print("!");
+         // .println displays the message THEN goes to next line
+
+         // ESCAPE: use \ (backslash) to include special characters, use \n for line break
+         String message = "\n I woke up and said, \"thank you beyonce\" ";
+         System.out.println(message);
+
+         // ARITHMETIC expressions with operators
+         System.out.println(4 + 3);
+         System.out.println(4 - 1);
+         System.out.println(6 * 7);
+         System.out.println(5 / 3); // TRUNCATED!
+         // Integer division results in TRUNCATION, which is basically throwing away the decimal places
+         System.out.println(5.0 / 3); // double instead of int
+
+         // COMOOUND expressions follow PEMDAS
+         double result = 2 + (3 * 2 - 6); 
+         System.out.println(result);
+
+         // REMAINER % operator 
+         int remain = 23 % 2; // 1 -> means ODD number
+         System.out.println(remain);
+         System.out.println(628 % 2); // 0 -> means EVEN number
+
+
       
    }
 }
